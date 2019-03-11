aac9745b155a93133a72f2e74619579eb12abb0d
Ckattik <Ckatt@ukr.net>
2019-03-11 15:12:01 +0200

    Added supper class Duck

---
+++ src/headFirst_SinUDack/Duck.java
@@ -0,0 +1,17 @@
+package headFirst_SinUDack;
+
+public class Duck {
+
+	public void quack() {
+		System.out.println("Krya, kray ");
+	}
+	
+	public void swim() {
+		System.out.println("Bul, bul ");
+	}
+	
+	public void display() {
+		System.out.println("see, see ");
+	}
+	
+}
