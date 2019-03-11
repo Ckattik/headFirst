596a2700dd7f1dba5b3f844ea4f39e1f04f5edd2
Vitaliy Mashkovskiy <Ckatt>
2019-03-11 11:11:27 +0200

    Start src @test@

---
+++ src/test/ATLSTFN.java
@@ -0,0 +1,10 @@
+package test;
+
+public class ATLSTFN {
+
+	public static void main(String []args) {
+		
+	}
+	
+	
+}
---
+++ src/test/ClassGIT.java
@@ -0,0 +1,10 @@
+package test;
+
+public class ClassGIT {
+
+	public static void main(String []args) {
+		System.out.println("Hello world");
+	}
+	
+	
+}
